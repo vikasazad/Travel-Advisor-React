74ffd225f9be2b63d5a4ca6a6d349f60eb33432f
Vikas Kumar Azad <surajnimeshh1000@gmail,com>
2021-12-03 08:42:02 +0530

    initial commit

---
+++ src/api/travelAdvisorAPI.js
@@ -0,0 +1,41 @@
+/* eslint-disable consistent-return */
+import axios from 'axios';
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'x-rapidapi-key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
+        'x-rapidapi-host': '3a05d60009msh66b4814fe5bad53p18d20djsn6197e4cc2ef5',
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
+        params: { lat, lon: lng },
+        headers: {
+          'x-rapidapi-key': process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
+          'x-rapidapi-host': '3a05d60009msh66b4814fe5bad53p18d20djsn6197e4cc2ef5',
+        },
+      });
+
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
